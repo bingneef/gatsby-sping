1047496239a46df80eb3da79d012dfe2e0f166a0
Bing Steup <bingsteup@gmail.com>
2019-01-25 17:04:23 +0100

    Fix?

--- bin/hello.js
+++ bin/hello.js
@@ -5,15 +5,6 @@ console.log(`
 
 ${chalk.green('Hey there! 👋')}
 
-Thanks for giving the ${pkg.name} a try. 🎉
-To get you going really quickly this project includes a setup step.
-
-${chalk.yellow.bold('yarn run setup')} automates the following steps for you:
-  - creates a config file ${chalk.yellow('./.contentful.json')}
-  - imports ${chalk.green('a predefined content model')}
-
-When this is done run:
-
 ${chalk.yellow(
   'yarn run dev'
 )} to start a development environment at ${chalk.green('localhost:8000')}
@@ -23,8 +14,4 @@ or
 ${chalk.yellow(
   'yarn run build'
 )} to create a production ready static site in ${chalk.green('./public')}
-
-For further information check the readme of the project
-(https://github.com/contentful-userland/gatsby-contentful-starter)
-
 `)
